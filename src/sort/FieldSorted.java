11c6defaec90c87c9d30649a8a8c0fb5a02e8805
Aliaksei Ramanau <111rao111@gmail.com>
2025-03-20 16:41:50 +0300

    Add Shell sort

--- src/sort/FieldSorted.java
+++ src/sort/FieldSorted.java
@@ -1,5 +1,22 @@
 package sort;
 
 public class FieldSorted {
+    public static <T extends Comparable<? super T>>
+    void shellsort( T [ ] a )
+    {
+        int j;
+        for( int gap = a.length / 2; gap > 0; gap /= 2 )
+        {
+            for( int i = gap; i < a.length; i++ )
+            {
+                T tmp = a[ i ];
+                for( j = i; j >= gap && tmp.compareTo( a[ j - gap ] ) < 0; j -= gap )
+                {
+                    a[ j ] = a[ j - gap ];
+                }
+                a[ j ] = tmp;
+            }
+        }
+    }
 
 }
