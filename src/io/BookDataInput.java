789b0dd2ec1cb4005e16a723b7e972a4a4a1d769
1 <1>
2025-03-21 14:47:04 +0300

    майн: добавлен тип ввода данных выход IO: проработан тип ввода данных (реализован только ручной ввод)

---
+++ src/io/BookDataInput.java
@@ -0,0 +1,36 @@
+package io;
+
+import data.Book;
+
+import java.util.Scanner;
+
+public class BookDataInput {
+
+    public static Book createBookFromConsole() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите автора книги (String): ");
+        String author = scanner.nextLine();
+
+        System.out.print("Введите название книги (String): ");
+        String title = scanner.nextLine();
+
+        System.out.print("Введите количество страниц (int): ");
+        int pages = scanner.nextInt();
+        scanner.nextLine(); // Очистка буфера
+
+        return new Book.Builder()
+                .setAuthor(author)
+                .setTitle(title)
+                .setPageCount(pages)
+                .build();
+    }
+    public static Book[] createBookArrayFromConsole(int length) {
+        Book[] books = new Book[length];
+        for (int i = 0; i < length; i++) {
+            System.out.println("Введите количество книг " + (i + 1) + ":");
+             books[i] = createBookFromConsole();
+        }
+        return books;
+    }
+}
---
+++ src/io/CarDataInput.java
@@ -0,0 +1,37 @@
+package io;
+
+import data.Car;
+import java.util.Scanner;
+
+public class CarDataInput {
+
+    public static Car createCarFromConsole() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите мощность (int): ");
+        int power = scanner.nextInt();
+        scanner.nextLine(); // Очистка буфера
+
+        System.out.print("Введите модель (String): ");
+        String model = scanner.nextLine();
+
+        System.out.print("Введите год изготовления (int): ");
+        int year = scanner.nextInt();
+        scanner.nextLine(); // Очистка буфера
+
+        return new Car.Builder()
+                .sethorsePower(power)
+                .setmodel(model)
+                .setreleaseYear(year)
+                .build();
+    }
+
+    public static Car[] createCarArrayFromConsole(int length) {
+        Car[] cars = new Car[length];
+        for (int i = 0; i < length; i++) {
+            System.out.println("Введите количество машин " + (i + 1) + ":");
+            cars[i] = createCarFromConsole();
+        }
+        return cars;
+    }
+}
