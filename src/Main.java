1b0f1408674d0645e7f1254fc2e829c5bcb064c5
Aliaksei Ramanau <111rao111@gmail.com>
2025-03-22 09:40:06 +0300

    Merge by main

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[1] args) {
         Scanner scanner = new Scanner(System.in);
         int choice;
 
