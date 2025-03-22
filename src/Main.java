722dbcf65904672aad4733e864d73e064465ada8
Aliaksei Ramanau <111rao111@gmail.com>
2025-03-22 22:57:42 +0300

    Delete 1

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[1] args) {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int choice;
 
